92eb19195dc260847491a51e998a884d34928214
fflorian25 <119571724+fflorian25@users.noreply.github.com>
2022-12-01 16:58:14 +0100

    Create day1_puzzle2.py

---
+++ day1_puzzle2.py
@@ -0,0 +1,26 @@
+'''
+Created on Dec 1, 2022
+
+@author: viennef1
+'''
+with open('input.txt', 'r') as reader:
+    line = reader.readline()
+    elf=[0]
+    
+    while line != '':                
+        if line.strip() != "":
+            elf[len(elf)-1] += int(line)            
+        else :
+             elf.append(0)
+             
+        line = reader.readline()
+   
+elf.sort(reverse=True)
+        
+print("food of top three  elf :" )
+print(elf[0])
+print(elf[1])
+print(elf[2])
+print("total food of top three  elf :" )
+print(elf[0]+ elf[1] +elf[2])
+    
