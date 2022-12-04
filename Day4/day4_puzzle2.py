b800763c9a04f71c964c2cde08918d727f6ca4db
Florian Viennet <fflorian25@wanadoo.fr>
2022-12-04 09:03:48 +0100

    Day4

---
+++ Day4/day4_puzzle2.py
@@ -0,0 +1,39 @@
+'''
+Created on Dec 2, 2022
+
+@author: viennef1
+'''
+import re
+import sys
+
+def findOverlap(sections1Beg, sections1End, sections2Beg, sections2End,):
+    if ( (sections1Beg <= sections2Beg) and (sections1End >= sections2Beg)):
+        return 1
+    elif ((sections2Beg <= sections1Beg) and (sections2End >= sections1Beg)):
+        return 1
+    else:
+        return 0
+
+with open('input_day4.txt', 'r') as reader:
+    line = reader.readline()
+    elf=[]    
+    while line != '':                
+        linespt =  line.strip("\n")
+        linespt= re.split(r'[,-]', linespt)
+        elf.append(linespt)  
+        
+        line = reader.readline()
+
+
+score=0
+
+#Loop on all
+for i in range(0,len(elf)):
+    #Return 1 if string 1 is contained into string 2 or opposite
+    score+=findOverlap(int(elf[i][0]), int(elf[i][1]), int(elf[i][2]), int(elf[i][3]))
+
+    
+print("the ultimate score is :")
+print(score)
+    
+    
