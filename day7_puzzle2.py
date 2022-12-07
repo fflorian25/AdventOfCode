ad99a2c42fd3e96a20be9324479b761a7e58e335
fflorian25 <119571724+fflorian25@users.noreply.github.com>
2022-12-07 19:21:06 +0100

    Create day7_puzzle2.py

---
+++ day7_puzzle2.py
@@ -0,0 +1,92 @@
+import copy
+
+class Folder():
+       
+    def __init__(self, parent, path):
+        self.parent=parent
+        self.path=path
+        self.folderChildList=[]
+        self.fileDict={}
+    
+    def addFolderChild(self, folderChild):
+        self.folderChildList.append(folderChild)
+
+    def addFile(self, fileName, fileSize):
+        self.fileDict[fileName]=fileSize
+        
+    def sizeFiles(self):
+        size = 0
+        for file, siz in self.fileDict.items():
+            size += siz     
+        return size
+    
+    def recurseSize(self):
+        totalSize = self.sizeFiles()
+        for elmt in self.folderChildList:
+            totalSize += elmt.recurseSize()
+        return totalSize
+        
+def pwd(beforePath, cmd):
+    beforePathCopy=copy.deepcopy(beforePath)
+    if ( cmd == ".."):
+        beforePathCopy.pop()
+        return beforePathCopy     
+    
+    else :
+        beforePathCopy.append(cmd)
+        return beforePathCopy
+    
+def toString(pwd):
+    strPwd = ""
+    if len(pwd)==1:
+        return "/"
+    for i in range(1,len(pwd)):
+            strPwd += "/" + pwd[i] 
+    return strPwd
+
+
+currentPath=[] 
+strCurrentPath=""
+allFolder={} 
+#create the root dir
+allFolder["/"] = Folder("root", "/")
+# read inputs
+with open('input_day7.txt', 'r') as reader:
+    line = reader.readline()
+    
+    while line != '':                     
+        linespt =  line.strip("\n").split()
+        
+        #creation of Path
+        if (linespt[0] == "$"):
+            if (linespt[1] == "cd"):
+                currentPath = pwd(currentPath, linespt[2] )
+                strCurrentPath = toString(currentPath)
+                
+        else:
+            #creation of folder
+            if (linespt[0] == "dir"):
+                strNewFolderPath=toString(pwd(currentPath, linespt[1]))
+                
+                allFolder[strNewFolderPath]=Folder(allFolder[strCurrentPath], pwd(currentPath, linespt[1]))
+                allFolder[strCurrentPath].addFolderChild(allFolder[strNewFolderPath])
+                
+            #else create file
+            else:
+                allFolder[strCurrentPath].addFile(linespt[1], int(linespt[0]))
+        
+        line = reader.readline()
+       
+SizeUsed=allFolder["/"].recurseSize()
+SizeAvailable=70000000-SizeUsed
+SizeToDelete=30000000-SizeAvailable
+SizeNear=SizeUsed
+for folder  in  allFolder:
+    sizeFolder=allFolder[folder].recurseSize()
+    if (sizeFolder > SizeToDelete) and (sizeFolder<SizeNear):
+        SizeNear=sizeFolder
+print("the ultimate score is :")
+print(SizeNear)
+
+    
+    
